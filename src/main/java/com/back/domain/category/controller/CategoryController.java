4d789cec31dc614ace519c6189d2ee0ca345f643
ehgk4245 <ehgkrk14@naver.com>
2025-11-13 11:46:03 +0900

    refactor: Swagger 코드 interface로 분리

--- src/main/java/com/back/domain/category/controller/CategoryController.java
+++ src/main/java/com/back/domain/category/controller/CategoryController.java
@@ -2,8 +2,6 @@ package com.back.domain.category.controller;
 
 import com.back.domain.category.dto.CategoryResBody;
 import com.back.domain.category.service.CategoryService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +13,10 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/categories")
-@Tag(name = "Category API", description = "카테고리 조회 API, 인증없이 접근 가능")
-public class CategoryController {
+public class CategoryController implements CategoryApi {
 
     private final CategoryService categoryService;
 
-    @Operation(summary = "카테고리 목록 조회 API", description = "카테고리들과 함께 연관된 하위 카테고리들 목록 조회")
     @GetMapping
     public ResponseEntity<List<CategoryResBody>> readCategories() {
         List<CategoryResBody> categoryResBodyList = categoryService.getCategories();
--- src/main/java/com/back/domain/region/controller/RegionController.java
+++ src/main/java/com/back/domain/region/controller/RegionController.java
@@ -2,8 +2,6 @@ package com.back.domain.region.controller;
 
 import com.back.domain.region.dto.RegionResBody;
 import com.back.domain.region.service.RegionService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +13,10 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/regions")
-@Tag(name = "Region API", description = "지역 조회 API, 인증없이 접근 가능")
-public class RegionController {
+public class RegionController implements RegionApi {
 
     private final RegionService regionService;
 
-    @Operation(summary = "지역 목록 조회 API", description = "지역들과 함께 연관된 하위 지역들 목록 조회")
     @GetMapping
     public ResponseEntity<List<RegionResBody>> readRegions() {
         List<RegionResBody> regionResBodyList = regionService.getRegions();
